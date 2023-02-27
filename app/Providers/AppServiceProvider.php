0ec369d93589bee7dfc0fc7757ecb6ff5db06de4
sabrina <trishaibtesham001@gmail.com>
2023-02-27 15:16:29 +0600

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrap();
+        View::share('recent_posts',\App\Models\Post::orderBy('id', 'desc')->limit('5')->get());
+    }
+}
